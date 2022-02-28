cdcec6b0767d599e002988c1e9cd20f0cf243ea8
xZifhuba <hafiirahman200@gmail.com>
2022-02-28 12:31:49 +0000

    React Router trial v1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
 import Content from './Content';
 import Menu from './Menu';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 
 function App() { //main App component containing sub-components of the page
   const title = 'AyahMatch';
   return (
     <div className="App">
-      
-      <Content />
-      <Menu />
-    
-      
+      <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/Surah1">Surah1</Link>
 
+      </nav>
+      <Routes>
+        <Route path="/" element={<><Content /><Menu /></>}/>
+        <Route path="/Surah1" element={<Content />}/>
+
+      </Routes>
+      
+   
+      
+      </BrowserRouter>
     </div>
     
   );
