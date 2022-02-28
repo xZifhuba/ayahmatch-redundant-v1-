97112836bf9a3f44b777f617648c0afe5d0ab3c6
xZifhuba <hafiirahman200@gmail.com>
2022-02-28 16:23:34 +0000

    route navigate feature

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import './App.css';
 import Content from './Content';
 import Menu from './Menu';
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'; //Importing react router terms
 
 function App() { //main App component containing sub-components of the page
   const title = 'AyahMatch';
   return (
     <div className="App">
-      <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/Surah1">Surah1</Link>
-
-      </nav>
-      <Routes>
+      <BrowserRouter /*Wrap entire div content with browser router for using route components*/> 
+      <Routes /*Routes tag used to contain all the invididual route paths to the pages, route tag is each individual path defined*/>
         <Route path="/" element={<><Content /><Menu /></>}/>
-        <Route path="/Surah1" element={<Content />}/>
+        <Route path="/Fatiha" element={<Content />}/>
 
       </Routes>
       
--- src/Menu.js
+++ src/Menu.js
@@ -3,6 +3,7 @@ import "slick-carousel/slick/slick-theme.css";
 import React, {Component} from "react";
 import Slider from "react-slick";
 import './Menu.css';
+import { useNavigate } from "react-router-dom";
 
 const Menu = () => {
     var settings = {
@@ -16,10 +17,11 @@ const Menu = () => {
         centerMode: true,
         arrows: false
     };
+    let navigate = useNavigate();
     return (
         <div className="Slide"> 
         <Slider {...settings}>
-            <button className="BtnMenu">Match1</button>
+            <button onClick={() => {navigate("/Fatiha")}} className="BtnMenu">Match1</button>
             <button className="BtnMenu">Match2</button>
             <button className="BtnMenu">Match3</button>
             <button className="BtnMenu">Match4</button>
