bc81bb011a6cfbdbd5ba033d671424748fb83eba
xZifhuba <hafiirahman200@gmail.com>
2022-03-06 14:59:50 +0000

    FatihaMatch setup + route fragment

---
+++ src/Pages/Fatiha/FatihaPage.js
@@ -0,0 +1,10 @@
+const cardImages = [] //array for storing the ayah images 
+
+
+const FatihaPage = () => {
+    return (
+       <button className="BtnHome" style={{float: 'right', top: '-50px', right: '500px'}}>New Game</button>
+      );
+}
+ 
+export default FatihaPage;
