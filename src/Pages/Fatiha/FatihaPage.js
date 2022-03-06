c58916409d27f2fc79a165e8f60a40a7da9959be
xZifhuba <hafiirahman200@gmail.com>
2022-03-06 22:16:12 +0000

    Fatiha ayah card grid front + back

--- src/Pages/Fatiha/FatihaPage.js
+++ src/Pages/Fatiha/FatihaPage.js
@@ -1,19 +1,21 @@
-import { useState } from "react"
+import { useState } from "react";
+import './../../Pages/Fatiha/FatihaPage.css';
+import cover from './../../img/cover.jpg';
 
-const cardImages = [
-    {"src": "/img/Fatiha-img/1.jpg"},
-    {"src": "/img/Fatiha-img/2.jpg"},
-    {"src": "/img/Fatiha-img/3.jpg"},
-    {"src": "/img/Fatiha-img/4.jpg"},
-    {"src": "/img/Fatiha-img/5.jpg"},
-    {"src": "/img/Fatiha-img/6.jpg"},
-    {"src": "/img/Fatiha-img/7.jpg"},
-    {"src": "/img/Fatiha-img/8.jpg"},
-    {"src": "/img/Fatiha-img/9.jpg"},
-    {"src": "/img/Fatiha-img/10.jpg"},
-    {"src": "/img/Fatiha-img/11.jpg"},
-    {"src": "/img/Fatiha-img/12.jpg"},
-    {"src": "/img/Fatiha-img/13.jpg"},
+const cardImages = [ //must use require due to webpack to properly locate path
+    {"src": require("./../../img/Fatiha-img/1.JPG")},
+    {"src": require("./../../img/Fatiha-img/2.JPG")},
+    {"src": require("./../../img/Fatiha-img/3.JPG")},
+    {"src": require("./../../img/Fatiha-img/4.JPG")},
+    {"src": require("./../../img/Fatiha-img/5.JPG")},
+    {"src": require("./../../img/Fatiha-img/6.JPG")},
+    {"src": require("./../../img/Fatiha-img/7.JPG")},
+    {"src": require("./../../img/Fatiha-img/8.JPG")},
+    {"src": require("./../../img/Fatiha-img/9.JPG")},
+    {"src": require("./../../img/Fatiha-img/10.JPG")},
+    {"src": require("./../../img/Fatiha-img/11.JPG")},
+    {"src": require("./../../img/Fatiha-img/12.JPG")},
+    {"src": require("./../../img/Fatiha-img/13.JPG")},
 ] //array for storing the ayah images 
 
 const FatihaPage = () => {
@@ -30,10 +32,25 @@ const FatihaPage = () => {
         setTurns(0)//
 
     }
-    console.log(cards, turns)
+
+   
 
     return (
+        <div>
+
        <button onClick={shuffleCards} className="BtnHome" style={{float: 'right', top: '-50px', right: '500px'}}>New Game</button>
+       <div className="card-grid"> 
+       {cards.map(card => ( //mapping through the each ayah card for displaying
+           <div className="card" key={card.id} /*div for card itself with the card id for randomizing*/>
+               <div /*sub div for front and back of each ayah card*/>
+                   <img className="front" src={card.src} alt="card front error" /*each mapped ayah card image*/ /> 
+                   <img className="back" src={cover} alt="card back error" /*back img for each mapped card*/ />
+               </div>
+           </div>
+       ))}
+
+       </div>
+       </div>
       );
 }
  
