2dd8c5987908622a98905b4edc88a149d749f013
xZifhuba <hafiirahman200@gmail.com>
2022-03-17 15:18:39 +0000

    Matching card features with card selection choice

--- src/Pages/Fatiha/SingleCard.js
+++ src/Pages/Fatiha/SingleCard.js
@@ -2,15 +2,22 @@ import cover from './../../img/cover.jpg'
 
 import './../../Pages/Fatiha/SingleCard.css'
 
-const SingleCard = ({card}) => {
+const SingleCard = ({card, handleChoice}) => {
+    const handleClick = () => {
+        handleChoice(card)
+    }
+
     return (  
     <div className="card" /*div for card element for styling*/>
         <div /*sub div for front and back of each ayah card*/>
             <img className="front" src={card.src} alt="card front error" /*each mapped ayah card image*/ /> 
-            <img className="back" src={cover} alt="card back error" /*back img for each mapped card*/ />
+            <img 
+            className="back" src={cover} 
+            onClick={handleClick} 
+            alt="card back error" /*back img for each mapped card*/ />
         </div>
     </div>
-    );
+    );  
 }
  
 export default SingleCard;
