18e9e3d28621bff0504442b24a84df6b94d6feb6
xZifhuba <hafiirahman200@gmail.com>
2022-04-17 16:55:22 +0100

    hamburger menu]

--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,6 @@
 import './Content.css';
 import { useNavigate } from 'react-router-dom';
+import Sidebar from './Sidebar';
 
 const Content = () => { //component for the page contents such as buttons, texts
     const title = 'AyahMatch';
@@ -7,6 +8,7 @@ const Content = () => { //component for the page contents such as buttons, texts
     return ( 
         <nav className='navbar'> 
         <div className='title'>AyahMatch</div>
+        <Sidebar />
         {/*<div className="Content">
             <h1 data-text="AyahMatch" style={{top: '5px'}}>{title}</h1>
             <button onClick={() => {navigate("/")}} className='BtnHome' style={{float: 'right', top: '-50px', right: '40px'}}>Home</button>
